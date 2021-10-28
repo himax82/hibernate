81fa37f483ed2eeeb4ea81358fb5c13ada9e4f57
himax82 <himax82@gmail.com>
2021-10-28 16:32:30 +0800

    0. HQL [#6874]

--- src/main/java/store/HbmRun.java
+++ src/main/java/store/HbmRun.java
@@ -1,6 +1,7 @@
 package store;
 
 import model.Brand;
+import model.Candidate;
 import model.Car;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -13,38 +14,38 @@ import java.util.List;
 public class HbmRun {
 
     public static void main(String[] args) {
-        List<Brand> list;
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure().build();
         try {
             SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
             Session session = sf.openSession();
             session.beginTransaction();
-            Brand honda = Brand.of("Honda");
-            Car crv = Car.of("CRV", honda);
-            Car accord = Car.of("Accord", honda);
-            Car pilot = Car.of("Pilot", honda);
-            Brand mazda = Brand.of("Mazda");
-            Car cx5 = Car.of("CX-5", mazda);
-            Car rx8 = Car.of("RX-8", mazda);
-            honda.addCar(crv);
-            honda.addCar(accord);
-            honda.addCar(pilot);
-            mazda.addCar(cx5);
-            mazda.addCar(rx8);
-            session.save(honda);
-            session.save(mazda);
-            session.save(crv);
-            session.save(accord);
-            session.save(pilot);
-            session.save(cx5);
-            session.save(rx8);
-            list = session.createQuery("from Brand ").list();
-            for (Brand brand : list) {
-                for (Car car : brand.getCars()) {
-                    System.out.println(car);
-                }
-            }
+
+            Candidate candidate1 = Candidate.of("Иван", "1 год", 80_000);
+            Candidate candidate2 = Candidate.of("Максим", "6 месяцев", 60_000);
+            Candidate candidate3 = Candidate.of("Евгений", "5 лет", 120_000);
+
+            session.save(candidate1);
+            session.save(candidate2);
+            session.save(candidate3);
+
+            List<Candidate> list = session.createQuery("from Candidate").list();
+            list.forEach(System.out::println);
+
+            Candidate evgen = (Candidate) session.createQuery("from Candidate c where c.salary = :sal")
+                    .setParameter("sal", "6 месяцев")
+                    .uniqueResult();
+            System.out.println(evgen);
+
+            session.createQuery("update from Candidate c set c.salary = :salary where c.id = :id")
+                    .setParameter("salary", 90_000)
+                    .setParameter("id", 1)
+                    .executeUpdate();
+
+            session.createQuery("delete from Candidate where id = :id")
+                    .setParameter("id", 1)
+                    .executeUpdate();
+
             session.getTransaction().commit();
             session.close();
         }  catch (Exception e) {

ce36ddec0ca404f774580cb53830653db749f7cb
himax82 <himax82@gmail.com>
2021-10-29 13:44:19 +0800

    Модели и связи. Машины и владельцы [#4744 #221603]

---
+++ src/main/java/maping/Driver.java
@@ -0,0 +1,55 @@
+package maping;
+
+import javax.persistence.*;
+import java.util.Objects;
+@Entity
+@Table(name = "driver")
+public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    public Driver() {
+    }
+
+    public Driver(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Driver driver = (Driver) o;
+        return id == driver.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/maping/Engine.java
@@ -0,0 +1,57 @@
+package maping;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "engine")
+public class Engine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    public Engine(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Engine() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Engine engine = (Engine) o;
+        return id == engine.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
